60570e1bb48b3b5a52e7399bff8e746de465c00d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-03 18:29:18 +0900

    fix: style Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin: 2rem 0;
+`;
 
-const Name = styled.h2``;
+const Title = styled.h2`
+  font-size: 1.5rem;
+  color: white;
+  margin-bottom: 0.5rem;
+`;
 
-const Info = styled.div``;
+const Subtitle = styled.span`
+  font-size: 0.98rem;
+  margin-left: 0.5rem;
+`;
 
-const Detail = styled.span``;
+const Info = styled.div`
+  margin-bottom: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid #495057;
+`;
 
-const Desc = styled.p``;
+const Detail = styled.span`
+  font-size: 1.1rem;
+  position: relative;
+  padding-right: 0.4rem;
+  margin-right: 0.5rem;
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: -2px;
+    transform: translateY(-50%);
+    width: 1.5px;
+    height: 1rem;
+    background: #495057;
+  }
+`;
+
+const Desc = styled.p`
+  font-size: 1rem;
+`;
 
 interface IProject {
-  name: string;
+  title: string;
+  subtitle?: string;
   part: string;
   event: string;
   date: string;
@@ -24,11 +58,14 @@ interface IProjectProps {
 }
 
 export default ({ project }: IProjectProps) => {
-  const { name, part, event, date, desc }: IProject = project;
+  const { title, subtitle, part, event, date, desc }: IProject = project;
 
   return (
     <Container>
-      <Name>{name}</Name>
+      <Title>
+        {title}
+        <Subtitle>{subtitle}</Subtitle>
+      </Title>
       <Info>
         <Detail>{part}</Detail>
         <Detail>{event}</Detail>
