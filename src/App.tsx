764512a7381fd4b5aae333046abbf01731f8fc1c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-03 16:54:38 +0900

    chore(init): package with yarn

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Header from './components/Header';
+
+const Layout = styled.div``;
+
+const App: React.FC = () => {
+  return (
+    <Layout className="App">
+      <Header />
+    </Layout>
+  );
+}
+
+export default App;
